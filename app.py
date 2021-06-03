f0ec893b58352e30b5d75cf156c65e8f39a4cad1
kamath7 <adithyakamath96@gmail.com>
2021-06-03 23:39:50 +0530

    Post route implementation TBD

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('index.html')
-
+#Need to implement form post route
 if __name__ == '__main__':
     app.run(debug=True)
